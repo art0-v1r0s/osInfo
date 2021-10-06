ebc0dbac022448c9ab1060e4acd7cafad4b80209
art0v1r0s <arthurd2000@gmail.com>
2021-10-06 12:15:01 +0200

    version windows

---
+++ main.py
@@ -0,0 +1,98 @@
+import platform
+import os
+from subprocess import check_output
+from xml.etree.ElementTree import fromstring
+from ipaddress import IPv4Interface, IPv6Interface
+import sys
+
+
+def get_interfaces():
+    cmd = 'wmic.exe nicconfig where "IPEnabled  = True" get ipaddress,MACAddress,IPSubnet,DNSHostName,Caption,DefaultIPGateway /format:rawxml'
+    xml_text = check_output(cmd, creationflags=8)
+    xml_root = fromstring(xml_text)
+
+    nics = []
+    keyslookup = {
+        'DNSHostName': 'hostname',
+        'IPAddress': 'ip',
+        'IPSubnet': '_mask',
+        'Caption': 'hardware',
+        'MACAddress': 'mac',
+        'DefaultIPGateway': 'gateway',
+    }
+
+    for nic in xml_root.findall("./RESULTS/CIM/INSTANCE"):
+        # parse and store nic info
+        n = {
+            'hostname': '',
+            'ip': [],
+            '_mask': [],
+            'hardware': '',
+            'mac': '',
+            'gateway': [],
+        }
+        for prop in nic:
+            name = keyslookup[prop.attrib['NAME']]
+            if prop.tag == 'PROPERTY':
+                if len(prop):
+                    for v in prop:
+                        n[name] = v.text
+            elif prop.tag == 'PROPERTY.ARRAY':
+                for v in prop.findall("./VALUE.ARRAY/VALUE"):
+                    n[name].append(v.text)
+        nics.append(n)
+
+        # creates python ipaddress objects from ips and masks
+        for i in range(len(n['ip'])):
+            arg = '%s/%s' % (n['ip'][i], n['_mask'][i])
+            if ':' in n['ip'][i]:
+                n['ip'][i] = IPv6Interface(arg)
+            else:
+                n['ip'][i] = IPv4Interface(arg)
+        del n['_mask']
+
+    return nics
+
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "-h":
+        print(
+            "###########################"
+            "#         INFO            #"
+            "#          OS             #"
+            "###########################"
+            "welcome in Info Os helper\n"
+            "this programme put the network info in a file\n"
+        )
+        exit()
+for root, directories, files in os.walk("."):
+    for file in files:
+        print(file)
+try:
+    with open('ipconfig.txt'):
+        pass
+except IOError:
+    lecteur = input("Si le fichier n'est pas a la racine du fichier peut -on le recré [O/N] : ")
+    if lecteur == "N":
+        exit()
+
+fichier = open("ipconfig.txt", 'w')
+OS = platform.system()
+print(OS)
+if "Windows" in OS:
+    print("it's Windows")
+    fichier.write("OS : %s \n" % OS)
+    nics = get_interfaces()
+    for nic in nics:
+        fichier.write('\n')
+        for k, v in nic.items():
+            fichier.write('%s : %s \n' % (k, v))
+            print('%s : %s' % (k, v))
+fichier.close()
+fichier = open("ipconfig.txt", "r")
+lignes = fichier.readlines()
+for ligne in lignes:
+    if "ip : " in ligne:
+        print(ligne)
+fichier.close()
+# ligne = ip.readline()
